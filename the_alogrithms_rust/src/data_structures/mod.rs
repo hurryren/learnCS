31116930627451956a1f7dacb4ea1511325304fc
orange <orange@format.com>
2021-11-15 16:50:04 +0800

    update

---
+++ the_alogrithms_rust/src/data_structures/mod.rs
@@ -0,0 +1,9 @@
+mod queue;
+mod linked_list;
+mod binary_search_tree;
+
+
+pub use self::queue::Queue;
+pub use self::linked_list::LinkedList;
+pub use self::binary_search_tree::BinarySearchTree;
+
---
+++ the_alogrithms_rust/src/main.rs
@@ -0,0 +1,14 @@
+// pub mod ciphers;
+pub mod data_structures;
+// pub mod dynamic_programming;
+// pub mod general;
+// pub mod graph;
+// pub mod math;
+// pub mod searching;
+// pub mod sorting;
+// pub mod string;
+
+
+fn main() {
+    println!("Hello, world!");
+}
